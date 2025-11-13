54b06e518a6ba1f3ee6c051309487be8ebc8fa15
GalaxyERPSoftware <galaxyerpsoftware@gmail.com>
2025-11-13 11:28:43 +0530

    add modified files

--- galaxynext/config/desktop.py
+++ galaxynext/config/desktop.py
@@ -1,14 +1,40 @@
+# def get_data():
+#     return [
+#         {
+#             "label": "Manufacturing",
+#             "items": [
+#                 {
+#                     "type": "page",
+#                     "name": "workstation-gantt",
+#                     "label": "Workstation Gantt",
+#                     "icon": "octicon octicon-calendar",
+#                 }
+#             ],
+#         }
+#     ]
+
+
+from frappe import _
+
 def get_data():
     return [
         {
-            "label": "Manufacturing",
-            "items": [
-                {
-                    "type": "page",
-                    "name": "workstation-gantt",
-                    "label": "Workstation Gantt",
-                    "icon": "octicon octicon-calendar",
-                }
-            ],
+            "module_name": "Manufacturing",
+            "category": "Modules",
+            "label": _("Manufacturing"),
+            "color": "#FFA00A",
+            "icon": "octicon octicon-package",
+            "type": "module",
+            "description": "Manufacturing and Production"
+        },
+        {
+            "module_name": "Workstation Gantt",
+            "category": "Places",
+            "label": _("Workstation Gantt"),
+            "type": "page",
+            "link": "workstation-gantt",
+            "icon": "octicon octicon-calendar",
+            "color": "#7289da",
+            "_label": _("Workstation Gantt View")
         }
     ]
